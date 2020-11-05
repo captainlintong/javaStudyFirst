1a1e4e787112a280474f0df1de277b36135c300e
captainlintong <lintong784533@163.com>
2020-11-06 02:45:25 +0800

    定义变量四种

--- firstdemo/myideatest/src/cn/com/csii/demo2/demo2.java
+++ firstdemo/myideatest/src/cn/com/csii/demo2/demo2.java
@@ -1,7 +1,33 @@
 package cn.com.csii.demo2;
 
 public class demo2 {
-    public static void main(String[] args) {
-        System.out.println("java学习第二天");
+    public static void main(String[] args)  {
+        System.out.println("java学习第二天了");
+        int a = 10;
+        double b = 10.2;
+        char c = 'a';
+        boolean d = true;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
     }
 }
+// 1、Notepad++安装
+// 2、注释 // /**/
+// 3、关键字 一共50左右个关键字 字母是小写的  编辑器里关键字会有颜色的变化
+// 4、常量
+/**
+ * 字符串常量 ""
+ * 整数常量 所有整数
+ * 小数常量 所有小数
+ * 字符常量 ''
+ * 布尔常量 true false
+ * 空常量 null
+ */
+
+/**
+ * 变量 就是内存中的存储空间，空间中存储着经常发生改变的量（数据）
+ * 数据类型  整数int 小数double 字符char 布尔boolean
+ */
