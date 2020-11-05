51000adfc9e8b4a78a076eda8a24c67fe83f4348
captainlintong <lintong784533@163.com>
2020-11-06 02:01:09 +0800

    Java学习第二天

---
+++ firstdemo/myideatest/src/cn/com/csii/demo2/demo2.java
@@ -0,0 +1,7 @@
+package cn.com.csii.demo2;
+
+public class demo2 {
+    public static void main(String[] args) {
+        System.out.println("java学习第二天");
+    }
+}
---
+++ firstdemo/myideatest/src/cn/com/csii/shortcut/demo1.java
@@ -0,0 +1,13 @@
+package cn.com.csii.shortcut;
+
+public class demo1 {
+    /*
+        psvm + 回车 : 快速生成main方法
+        sout + 回车 : 快速生成输出语句
+        alt + 1 : 打开 / 隐藏工程目录结构
+        alt + 4 : 打开 / 隐藏控制台
+     */
+    public static void main(String[] args) {
+        System.out.println("快捷键");
+    }
+}
